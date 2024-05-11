ad4464ab61f1b5bf8fa8b8abbb519be39d258aec
MrBetaMax <dewaldifels@gmail.com>
2024-05-11 15:43:47 +0200

    feat: Colour scheme

--- lua/mrbetamax/packer.lua
+++ lua/mrbetamax/packer.lua
@@ -12,4 +12,11 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use {
+	 "folke/tokyonight.nvim",
+	 lazy = false,
+	 priority = 1000,
+	 opts = {}
+  }
 end)
